73fda940735bf99c30406205d3183ba0132c8b8a
CowsCanProgram <bobthecow888@gmail.com>
2024-04-15 12:14:21 -0400

    hi

--- sugarcane.py
+++ sugarcane.py
@@ -1,32 +1,30 @@
 import pyautogui
 import time
 # Example: Hold down the 'a' key for 3 seconds
-time.sleep(5)
-for i in range(0, 15, 1):
-    try:
-        with pyautogui.hold('option'):
-            pyautogui.keyDown('a')
-            time.sleep(50)
-            pyautogui.keyUp('a')
-        with pyautogui.hold('option'):
-            pyautogui.keyDown('s')
-            time.sleep(50)
-            pyautogui.keyUp('s')
-    except KeyboardInterrupt:
-        print('Done')
-        break
-pyautogui.press('/')
-pyautogui.press('w')
-pyautogui.press('a')
-pyautogui.press('r')
-pyautogui.press('p')
-pyautogui.press('space')
-pyautogui.press('g')
-pyautogui.press('a')
-pyautogui.press('r')
-pyautogui.press('d')
-pyautogui.press('e')
-pyautogui.press('n')
-pyautogui.press('return')
-
-print('Done')
+try:
+    time.sleep(5)
+    pyautogui.keyDown('option')
+    for i in range(0, 15, 1):
+        pyautogui.keyDown('a')
+        time.sleep(48.5)
+        pyautogui.keyUp('a')
+        pyautogui.keyDown('s')
+        time.sleep(48.7)
+        pyautogui.keyUp('s')
+    pyautogui.keyUp('option')
+    pyautogui.press('/')
+    pyautogui.press('w')
+    pyautogui.press('a')
+    pyautogui.press('r')
+    pyautogui.press('p')
+    pyautogui.press('space')
+    pyautogui.press('g')
+    pyautogui.press('a')
+    pyautogui.press('r')
+    pyautogui.press('d')
+    pyautogui.press('e')
+    pyautogui.press('n')
+    pyautogui.press('return')
+except KeyboardInterrupt:
+    print('Done')
+        

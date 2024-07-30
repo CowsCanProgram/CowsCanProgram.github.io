f60c8545f1a57e6d91fb82683ffcf16ab74ca861
CowsCanProgram <bobthecow888@gmail.com>
2024-07-30 15:53:16 -0400

    Functioning Now

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, session, jsonify
 import random
 import urllib.parse
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 equations = {
     r'E=mc^2': 'Einstein Equation',
@@ -17,13 +18,27 @@ def index():
 
 @app.route('/math')
 def math():
+    if 'correct_count' not in session:
+        session['correct_count'] = 0
+    if 'incorrect_count' not in session:
+        session['incorrect_count'] = 0
+
     image_url = request.args.get('image', '')
     equation = request.args.get('equation', '')
     result = request.args.get('result', '')
     correct_answer = request.args.get('correct_answer', '')
     new_image_url = request.args.get('new_image', '')
     new_equation = request.args.get('new_equation', '')
-    return render_template('math.html', image_url=image_url, equation=equation, result=result, correct_answer=correct_answer, new_image_url=new_image_url, new_equation=new_equation)
+
+    return render_template('math.html', 
+                           image_url=image_url, 
+                           equation=equation, 
+                           result=result, 
+                           correct_answer=correct_answer, 
+                           new_image_url=new_image_url, 
+                           new_equation=new_equation,
+                           correct_count=session['correct_count'],
+                           incorrect_count=session['incorrect_count'])
 
 
 
@@ -49,7 +64,7 @@ def random_math():
     equation = random.choice(list(equations.keys()))
     encoded_equation = urllib.parse.quote_plus(equation)
     image_url = f'https://latex.codecogs.com/svg.latex?{encoded_equation}'
-    return redirect(url_for('math', new_image=image_url, new_equation=equation))
+    return redirect(url_for('math', image=image_url, equation=equation))
 
 @app.route('/check_answer', methods=['POST'])
 def check_answer():
@@ -59,17 +74,29 @@ def check_answer():
 
     if user_answer == correct_answer:
         result = 'Correct'
-        correct_answer = ''
+        session['correct_count'] = session.get('correct_count', 0) + 1
     else:
         result = 'Incorrect'
+        session['incorrect_count'] = session.get('incorrect_count', 0) + 1
 
+    # Generate a new random equation for the next question
     new_equation = random.choice(list(equations.keys()))
     encoded_new_equation = urllib.parse.quote_plus(new_equation)
     new_image_url = f'https://latex.codecogs.com/svg.latex?{encoded_new_equation}'
 
-    encoded_equation = urllib.parse.quote_plus(equation)
-    image_url = f'https://latex.codecogs.com/svg.latex?{encoded_equation}'
-    return redirect(url_for('math', image=image_url, equation=equation, result=result, correct_answer=correct_answer, new_image=new_image_url, new_equation=new_equation))
+    return redirect(url_for('math', 
+                            image=new_image_url, 
+                            equation=new_equation, 
+                            result=result, 
+                            correct_answer=correct_answer, 
+                            new_image=new_image_url, 
+                            new_equation=new_equation))
+
+    
+@app.route('/reset_session')
+def reset_session():
+    session.clear()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
